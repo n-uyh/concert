adea8ec6db233da7727b892aa87537d443a0fd4c
hyun <edocnuyh@gmail.com>
2024-10-18 06:25:34 +0900

    docs: swagger 수정

--- src/main/java/com/hhplus/concert/api/payment/IPaymentController.java
+++ src/main/java/com/hhplus/concert/api/payment/IPaymentController.java
@@ -3,8 +3,10 @@ package com.hhplus.concert.api.payment;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
+@Tag(name = "Payment", description = "결제 API")
 public interface IPaymentController {
 
     @Operation(summary = "예약내역 결제 요청", description = "예약내역에 관한 결제를 요청합니다.")
