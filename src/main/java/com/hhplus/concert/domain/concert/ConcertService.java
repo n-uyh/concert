3b2fdc5c2c96cb20db3e47e63bf8e8f9c2aad228
hyun <edocnuyh@gmail.com>
2024-11-08 00:59:15 +0900

    docs: 캐싱 정리 및 적용기

--- src/main/java/com/hhplus/concert/domain/concert/ConcertService.java
+++ src/main/java/com/hhplus/concert/domain/concert/ConcertService.java
@@ -2,6 +2,7 @@ package com.hhplus.concert.domain.concert;
 
 import com.hhplus.concert.domain.concert.ConcertException.ConcertError;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
@@ -16,7 +17,7 @@ public class ConcertService {
     @Cacheable(key = "#command", value = "availableConcerts")
     public List<ConcertInfo.Common> findAvailable(ConcertCommand.Available command) {
         List<ConcertEntity> concerts = concertRepository.findAvailable(command.from(), command.end());
-        return concerts.stream().map(ConcertInfo.Common::of).toList();
+        return concerts.stream().map(ConcertInfo.Common::of).collect(Collectors.toList());
     }
 
     public List<ConcertInfo.SeatInfo> findAvailableConcertSeats(ConcertCommand.Seat command) {
