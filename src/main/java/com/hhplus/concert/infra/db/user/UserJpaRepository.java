de02653ef7d18b071c1eb40fa60ca7e96648e2c6
hyun <edocnuyh@gmail.com>
2024-11-10 20:53:43 +0900

    refactor: 안쓰는 클래스 정리

--- src/main/java/com/hhplus/concert/infra/db/user/UserJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hhplus.concert.infra.db.user;
-
-import com.hhplus.concert.domain.user.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserJpaRepository extends JpaRepository<UserEntity, Long> {
-}
